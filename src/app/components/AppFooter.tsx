8ed4ad55eca2f5a3f2fada725ca9da2401c95e95
Jae Webb <jynnae.webb@gmail.com>
2025-03-17 18:35:47 -0400

    escape the '

--- src/app/components/AppFooter.tsx
+++ src/app/components/AppFooter.tsx
@@ -6,7 +6,7 @@ export default function AppFooter() {
       <Box sx={{ pt: '2rem', textAlign: 'center' }}>
         &copy;{ new Date().getFullYear() } Jae Webb<br />
         data provided by <a href="https://highlightly.net">Highlightly</a><br />
-        If there's no data then I've reached my basic plan limit for the day!
+        If there&apos;s no data then I&apos;ve reached my basic plan limit for the day!
       </Box>
     </footer>
   )
